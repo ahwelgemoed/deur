69d9d665372698b717ef3e10a8e39023c894c91b
ahwelgemoed <arno.welgemoed@outlook.com>
2023-01-21 17:20:37 +0100

    fixes

--- apps/base-local/src/index.ts
+++ apps/base-local/src/index.ts
@@ -1,48 +1,13 @@
-import Fastify from 'fastify';
-import FastifySwagger from '@fastify/swagger';
-import { withRefResolver } from 'fastify-zod';
-import gateRoutes from './modules/gate/gate.route';
-import { gateLocalSchemas } from './modules/gate/gate.schema';
-import { version } from '../package.json';
+import { bootstrap } from './server';
 
-gateLocalSchemas;
-
-const server = Fastify();
-
-server.get('/healthcheck', async (request, reply) => {
-  return { status: 'OK' };
-});
+const server = bootstrap();
 
 async function main() {
-  // Add all schemas to the server
-  for (const schema of [...gateLocalSchemas]) {
-    server.addSchema(schema);
-  }
-
-  server.register(
-    FastifySwagger,
-    withRefResolver({
-      routePrefix: '/swagger',
-      swagger: {
-        info: {
-          title: 'Gate API',
-          description: 'Gate API',
-          version: version,
-        },
-      },
-    })
-  );
-  server.register(require('@fastify/swagger-ui'), {
-    routePrefix: '/swagger',
-  });
-
-  // Register all routes
-  server.register(gateRoutes, { prefix: '/gate' });
   try {
     await server.listen({ port: Number('3034'), host: '0.0.0.0' });
     console.log('🚀 Server listening on port 3034');
-  } catch (err) {
-    server.log.error(err);
+  } catch (e) {
+    console.error(e);
     process.exit(1);
   }
 }
---
+++ apps/base-local/src/server.ts
@@ -0,0 +1,41 @@
+import Fastify, { FastifyInstance } from 'fastify';
+import FastifySwagger from '@fastify/swagger';
+import { withRefResolver } from 'fastify-zod';
+import gateRoutes from './modules/gate/gate.route';
+import { gateLocalSchemas } from './modules/gate/gate.schema';
+import { version } from '../package.json';
+
+export function bootstrap(): FastifyInstance {
+  const server = Fastify();
+
+  server.get('/healthcheck', async (request, reply) => {
+    return { status: 'OK' };
+  });
+
+  // Add all schemas to the server
+  for (const schema of [...gateLocalSchemas]) {
+    server.addSchema(schema);
+  }
+
+  server.register(
+    FastifySwagger,
+    withRefResolver({
+      routePrefix: '/swagger',
+      swagger: {
+        info: {
+          title: 'Gate API',
+          description: 'Gate API',
+          version: version,
+        },
+      },
+    })
+  );
+  server.register(require('@fastify/swagger-ui'), {
+    routePrefix: '/swagger',
+  });
+
+  // Register all routes
+  server.register(gateRoutes, { prefix: '/gate' });
+
+  return server;
+}
